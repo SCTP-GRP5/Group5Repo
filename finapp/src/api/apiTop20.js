615323357763dcfff00231496babb2acebeea75c
Stella Ferdinand <stellaferdinand1998@gmail.com>
2023-10-14 04:10:53 +0800

    top 20 updated

--- finapp/src/api/apiTop20.js
+++ finapp/src/api/apiTop20.js
@@ -2,7 +2,7 @@
 
 const apiTop20 = {
   method: 'GET',
-  url: 'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=A6P25Z6PDO8RC55S',
+  url: 'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=c70f00d4d2msh0c3534ff1489e05p1c6651jsnbfc0aaba8fc7',
 }
 
 export default apiTop20;
--- finapp/src/components/Top20Table.js
+++ finapp/src/components/Top20Table.js
@@ -2,29 +2,24 @@ import Top20MostActive from "./Top20MostActive";
 import Top20Gainers from "./Top20Gainers";
 import Top20Losers from "./Top20Losers";
 
-function Top20Table({ list }) {
-  const handlerDisplayChart = (id) => {
-    return console.log("chart displayed");
-  };
+
+
+
+function Top20Table({ list = [], chartData }) {
 
   return (
     <div>
-      <h3>Top 20 Most Active</h3>
-      <Top20MostActive
+{    console.log(list)
+}      <Top20MostActive
         data={list.most_actively_traded}
-        handlerDisplayChart={handlerDisplayChart}
       />
-      <hr></hr>
-      <h3>Top 20 Gainers</h3>
+      
       <Top20Gainers
         data={list.top_gainers}
-        handlerDisplayChart={handlerDisplayChart}
       />
-      <hr></hr>
-      <h3>Top 20 Losers</h3>
+      
       <Top20Losers
         data={list.top_losers}
-        handlerDisplayChart={handlerDisplayChart}
       />
     </div>
   );
