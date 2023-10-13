c0e9b9f4399e1c16f5cfe86145c5035b396c22bd
saranya <saran25@gmail.com>
2023-10-13 09:46:20 +0800

    Updated Forex Component

--- finapp/src/components/Forex.js
+++ finapp/src/components/Forex.js
@@ -1,7 +1,7 @@
 import styles from './Forex.module.css';
 import { useState, useEffect } from 'react';
 import ForexTable from "./ForexTable";
-//import ForexChart from "./ForexChart";
+import ForexChart from "./ForexChart";
 import axios from "axios";
 
 
@@ -25,50 +25,50 @@ headers: {
 
 const [data, setData] = useState([]);
 const [fxmetadata, setFxmetadata] = useState([]);
-
+const [fxchartData, setFxchartData] = useState([]);
 
 const [isLoading, setIsLoading] = useState(false);
 
 // for choosing currency options
 const currencyList = [
  { 
-   name: "Singapore Dollar",
+   name: " 🇸🇬 Singapore Dollar",
    value: "SGD",
   },
  {
-    name: "US Dollar",
+    name: " 🇺🇸 US Dollar",
     value: "USD",
  },
  {
-   name: "EURO",
+   name: " 🇪🇺 EURO",
    value: "EUR",
  },
  {
-   name: "Japanese Yen",
+   name: " 🇯🇵 Japanese Yen",
    value: "JPY"
  },
  {
-  name: "British Pound",
+  name: " 🇬🇧 British Pound",
   value:"GBP"
  },
  {
-  name: "Indian Rupees",
+  name: " 🇮🇳 Indian Rupees",
   value: "INR"
  },
  {
-  name:"Malaysian Ringgit",
+  name:" 🇲🇾 Malaysian Ringgit",
   value: "MYR"
  },
  {
-   name:"Chinese Yuan",
+   name:" 🇨🇳 Chinese Yuan",
    value:"CNY"
  },
  {
-   name:"Russian Ruble",
+   name:" 🇷🇺 Russian Ruble",
    value:"RUB"
  },
  {
-  name:"United Arab Diraham",
+  name:" 🇦🇪 United Arab Diraham",
   value: "AED"
  },
 ];
@@ -79,13 +79,22 @@ const currencyList = [
     try {
       const response = await axios.request(forexAPI);
       console.log(response.data["Time Series FX (Daily)"]);
-      
-     // console.log((response["Time Series FX (Daily)"]).length);
+
       setData(response.data["Time Series FX (Daily)"]);   
-      setFxmetadata(response.data["Meta Data"]);
-      console.log(response.data["Meta Data"])
-      console.log(fxmetadata);
-      console.log(response.data); 
+      
+      const fxchartData = Object.keys(response.data['Time Series FX (Daily)']).map(date => ({
+        date,
+        '1. open': parseFloat(response.data['Time Series FX (Daily)'][date]['1. open']),
+        '2. high': parseFloat(response.data['Time Series FX (Daily)'][date]['2. high']),
+        '3. low': parseFloat(response.data['Time Series FX (Daily)'][date]['3. low']),
+        '4. close': parseFloat(response.data['Time Series FX (Daily)'][date]['4. close']),
+      }));
+      
+     setFxmetadata(response.data["Meta Data"]);
+     setFxchartData(fxchartData);
+
+      console.log(fxchartData);
+      
     } catch (error) {
       console.log("❌ Error:" +error.message);
     } finally {
@@ -108,8 +117,9 @@ const currencyList = [
 
   return (
     <div className={ styles.Forex }>
-      <h1>Product List</h1>
+      <h1>FOREX CURRENCY EXCHANGE RATES</h1>
 
+      <div className={styles.controls}>
       <select
           className={styles.select}
           name="from_symbol"
@@ -137,11 +147,12 @@ const currencyList = [
           ))}
           <button type="submit">Submit</button>
         </select>
+        </div>
       <button  className={styles.button} onClick={apiGet}> Load Forex </button>
       {isLoading && <p> ⏳ Loading...</p>}
       {data && fxmetadata && <ForexTable data ={data} fxmetadata = {fxmetadata} /> }
       <hr></hr>
-      {/* {data && fxmetadata && <ForexChart data ={data} fxmetadata = {fxmetadata} /> } */}
+      {fxchartData && <ForexChart fxchartData ={fxchartData} /> }
 
     </div>
   );
